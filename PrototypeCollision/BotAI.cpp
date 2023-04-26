e77e01b15b90a6491d394b1fd03b6efc09f0dce6
mj <mikulej7@gmail.com>
2023-04-26 20:47:22 +0200

    MCTS Simulations based on proper all in answers

--- PrototypeCollision/BotAI.cpp
+++ PrototypeCollision/BotAI.cpp
@@ -1,7 +1,7 @@
 #include "BotAI.h"
 std::vector<Karta> Point::hand_cards;
 std::vector<Karta> Point::desk_cards;
-#define SIM_NUM 5000
+#define SIM_NUM 500
 Point::Point(const std::vector<Karta>& _hand_cards, const std::vector<Karta>& _desk_cards, bool _isDealer, int _bot_credits,int _bot_gave, int _enemy_credits, int _enemy_gave, bool _enemy_checks,  int _pool,int _checked_cards) {//head constructor
 	hand_cards = _hand_cards;
 	desk_cards = _desk_cards;
@@ -17,7 +17,7 @@ Point::Point(const std::vector<Karta>& _hand_cards, const std::vector<Karta>& _d
 
 	Point* node_current = this;//zacznij od punktu startowego(glowy)
 	while (simulations < SIM_NUM) {
-		//std::cout << simulations << std::endl;
+		//std::cout << "SIM:"<<simulations << std::endl;
 		node_current->expansion();//dodaj 3 rozgalezienia
 		bool won_sim; //wykonaj ich symulacje
 		won_sim = node_current->node_raise->simulation();
@@ -29,6 +29,7 @@ Point::Point(const std::vector<Karta>& _hand_cards, const std::vector<Karta>& _d
 		node_current = selection();//wybierz nowe rozgalezienie do obslugi
 		
 	}
+	std::cout << "Simulations done:" << simulations << std::endl;
 	//wybierz najlepszy ruch (ten z najwieksza iloscia symulacji)
 	//zaktualizuj SimData dla korzenia bedzie to ruch ktory jako nastepny wykona bot
 	int max_sim = node_raise->simulations; sd = node_raise->sd;
@@ -100,6 +101,7 @@ bool Point::simulation(void) {
 	int checked_cards = sd.checked_cards;
 	int missing_blind = false;
 	bool isDealer = sd.isDealer;
+	bool last_move = false;
 	while (players[0].get_credits()!=0 || players[1].get_credits() !=0) {//bot wykonuje pierwszy ruch
 		std::vector<Gracz>::iterator player = players.begin(); if (bot_turn) { player++; }
 		std::vector<Gracz>::iterator enemy = players.begin(); if (!bot_turn) { enemy++; }
@@ -109,6 +111,9 @@ bool Point::simulation(void) {
 		//blinds
 		if (round_start) {		
 			//std::cout << "New round " << std::endl;
+			last_move = false;
+			players[0].allin = false;
+			players[1].allin = false;
 			if (!isDealer) {//dealer dostaje big blind
 				if (players[0].get_credits() >= 200) { players[0].sim_give_to_pool(200, sim_pool); }
 				else { missing_blind = true; }
@@ -121,84 +126,98 @@ bool Point::simulation(void) {
 				else{ missing_blind = true; }
 				if(players[1].get_credits() >= 200){ players[1].sim_give_to_pool(200, sim_pool); }
 				else{ missing_blind = true; }
-
 				//std::cout << "Bot is dealer " << std::endl;
 			}
+			if (players[0].get_credits() == 0) { players[0].allin = true; }
+			if (players[1].get_credits() == 0) { players[1].allin = true; }
 			round_start = false;
 		}
 		if (missing_blind) { break; }
 		//std::cout << "Bot credits=" << players[1].get_credits() << " Player credits=" << players[0].get_credits() << " sim_pool=" << sim_pool << " all=" << players[1].get_credits() + players[0].get_credits() + sim_pool << std::endl;
-		if ((*player).get_credits() == 0 || (*enemy).get_credits() == 0) {//forced check
-			checked_cards = 4;
-		}
-		else {
-			//perform random action
-			int option;
-			if (!updated_sim_data) { option = first_move;  } //wykonaj pierwszy ruch zaplanowany w klasie Point
-			else{ option = rand() % 3; }//moze ustawic jakies lepsze wagi? ze np fold ma 65% szans			
-			int raise_amount; int h_max; int h_min; int align_amount; int fin_min_raise = 0; int h;
-			switch (option) {
-			case 0://raise - done
-				if (!updated_sim_data) {
-					raise_amount = sd.raise_by;
-				}
-				else{
-					fin_min_raise = (*enemy).gave_to_pool - (*player).gave_to_pool;
-					if (fin_min_raise < 0) { fin_min_raise = 0; }
-					fin_min_raise += Game::minimal_raise;
-
-					h_min = fin_min_raise;
-					h_max = (*player).get_credits();
-					h = h_max - h_min;
-					if (h <= 0) { raise_amount = h_max; }
-					else {
-						raise_amount = (rand() % h / 100) * 100 + h_min;
-					}
-				}
-				
-
-				(*player).sim_give_to_pool(raise_amount, sim_pool);
-				(*player).checks = false; (*enemy).checks = false;
-				//std::cout << bot_turn << " raised by " << raise_amount << std::endl;
-				break;
-			case 1://call - done		
-				//jesli caller dal mniej niz drugi to musi doplacic
-				if ((*player).gave_to_pool < (*enemy).gave_to_pool) {
-					align_amount = (*enemy).gave_to_pool - (*player).gave_to_pool;
-					if (align_amount < (*player).get_credits()) {//ma wystarczajaco kredytow by wyrownac stawke
-						(*player).sim_give_to_pool((*enemy).gave_to_pool - (*player).gave_to_pool, sim_pool);
-					}
-					else {//nie ma
-						(*player).sim_give_to_pool((*player).get_credits(), sim_pool);
-					}
-				}
-				(*player).checks = true;
-				//std::cout << bot_turn << " calls " << std::endl;
-				break;
-			case 2://fold
-				(*enemy).add_credits(sim_pool);
-				sim_pool = 0; (*player).gave_to_pool = 0; (*enemy).gave_to_pool = 0;
-				(*player).checks = false; (*enemy).checks = false;
-				//std::cout << bot_turn << " folds " << std::endl;
-				bot_turn = !bot_turn;
-				round_start = true;
-				isDealer = !isDealer;
-				if (!updated_sim_data) { //zwroc SimulationData po wykonaniu pierwszego ruchu do Point
-					sd.enemy_credits = players[0].get_credits();
-					sd.bot_credits = players[1].get_credits();
-					sd.enemy_gave = players[0].gave_to_pool;
-					sd.bot_gave = players[1].gave_to_pool;
-					sd.pool = sim_pool;
-					sd.checked_cards = checked_cards;
-					sd.isDealer = isDealer;
-					updated_sim_data = true;
-				}
-				continue;//dla optymalizacji ale jesli psuje switcha to mozna wywalic i te bot_turn = !bot_turn; tez
-				break;
+		if ((*enemy).allin) {
+			//if (bot_turn) { std::cout << "Bot answers to all in" << std::endl; }
+			//else { std::cout << "Player answers to all in" << std::endl; }
+			last_move = true;
+		}	
+		//perform random action
+		int option;
+		if (!updated_sim_data) { option = first_move;  } //wykonaj pierwszy ruch zaplanowany w klasie Point
+		else//moze ustawic jakies lepsze wagi? ze np fold ma 65% szans	
+		{ 
+			if ((*enemy).allin) {//bot nie bedzie podbijal stawki gdy gracz poszedl all in - nie ma sensu podbijac stawki
+				option = (rand() % 2) + 1;
 			}
-			if ((*player).checks && (*enemy).checks) { checked_cards++; }
+			else {
+				option = rand() % 3;
+			}
+		}		
+		int raise_amount; int h_max; int h_min; int align_amount; int fin_min_raise = 0; int h;
+		switch (option) {
+		case 0://raise - done
+			if (!updated_sim_data) {
+				raise_amount = sd.raise_by;
+			}
+			else{
+				fin_min_raise = (*enemy).gave_to_pool - (*player).gave_to_pool;
+				if (fin_min_raise < 0) { fin_min_raise = 0; }
+				fin_min_raise += Game::minimal_raise;
+
+				h_min = fin_min_raise;
+				h_max = (*player).get_credits();
+				h = h_max - h_min;
+				if (h <= 0) { raise_amount = h_max; }
+				else {
+					raise_amount = (rand() % h / 100) * 100 + h_min;
+				}
+			}
+				
+			if (raise_amount == (*player).get_credits()) { (*player).allin = true; }
+			(*player).sim_give_to_pool(raise_amount, sim_pool);
+			(*player).checks = false; (*enemy).checks = false;
+			//std::cout << bot_turn << " raised by " << raise_amount << std::endl;
+			break;
+		case 1://call - done		
+			//jesli caller dal mniej niz drugi to musi doplacic
+			if ((*player).gave_to_pool < (*enemy).gave_to_pool) {
+				align_amount = (*enemy).gave_to_pool - (*player).gave_to_pool;
+				if (align_amount < (*player).get_credits()) {//ma wystarczajaco kredytow by wyrownac stawke
+					(*player).sim_give_to_pool((*enemy).gave_to_pool - (*player).gave_to_pool, sim_pool);
+				}
+				else {//nie ma
+					(*player).sim_give_to_pool((*player).get_credits(), sim_pool);
+					(*player).allin = true;
+				}
+			}
+			(*player).checks = true;
+			//std::cout << bot_turn << " calls " << std::endl;
+			break;
+		case 2://fold
+			(*enemy).add_credits(sim_pool);
+			sim_pool = 0; (*player).gave_to_pool = 0; (*enemy).gave_to_pool = 0;
+			(*player).checks = false; (*enemy).checks = false;
+			//std::cout << bot_turn << " folds " << std::endl;
+			bot_turn = !bot_turn;
+			round_start = true;
+			isDealer = !isDealer;
+			if (!updated_sim_data) { //zwroc SimulationData po wykonaniu pierwszego ruchu do Point
+				sd.enemy_credits = players[0].get_credits();
+				sd.bot_credits = players[1].get_credits();
+				sd.enemy_gave = players[0].gave_to_pool;
+				sd.bot_gave = players[1].gave_to_pool;
+				sd.pool = sim_pool;
+				sd.checked_cards = checked_cards;
+				sd.isDealer = isDealer;
+				updated_sim_data = true;
+			}
+			continue;//dla optymalizacji ale jesli psuje switcha to mozna wywalic i te bot_turn = !bot_turn; tez
+			break;
 		}
+		if ((*player).checks && (*enemy).checks) { checked_cards++; }
 		
+		if (last_move) {
+			checked_cards = 4;
+			last_move = false;
+		}
 		if (checked_cards == 4) {
 			players[0].calculate_power(); players[1].calculate_power();
 			//porownaj i wybierz zwyciesce
@@ -246,7 +265,6 @@ bool Point::simulation(void) {
 		//std::cout << "BOT LOST" << std::endl; 
 		return false;
 	}
-
 }
 void Point::expansion(void) {
 	SimData nsd = sd;
